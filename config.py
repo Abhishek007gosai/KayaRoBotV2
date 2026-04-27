001a09885f57a8336d7a4a5b5c96c6b36629bc4f
Abhishek007gosai <99321716+Abhishek007gosai@users.noreply.github.com>
2026-04-27 17:37:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,26 +4,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = int(os.environ.get("API_ID", ))
-API_HASH = os.environ.get("API_HASH", "")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+API_ID = 29245477
+API_HASH = ("0abc83883262245c90ca337b7a0375c4")
+BOT_TOKEN = ("8691693420:AAGwvJY1A_gLRNueXE17I1A88-R2kSIQ3mg")
 
-SET_INTERVAL = int(os.environ.get("SET_INTERVAL", 60))  # in seconds, default 1 hour
-TARGET_CHAT_ID = os.environ.get("TARGET_CHAT_ID", "")
-MAIN_CHANNEL = os.environ.get("MAIN_CHANNEL", "") # Change as needed
-LOG_CHANNEL = os.environ.get("LOG_CHANNEL", "")
-MONGO_URL = os.environ.get("MONGO_URL", "")
-MONGO_NAME = os.environ.get("MONGO_NAME", "cantarellabots")
-OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-ADMIN_URL = os.environ.get("ADMIN_URL", "@V_Sbotmaker")
-BOT_USERNAME = os.environ.get("BOT_USERNAME", "")
+SET_INTERVAL = int(os.environ.get("SET_INTERVAL", 60))                              
+TARGET_CHAT_ID = -1002512442401
+MAIN_CHANNEL = -1003932217304
+LOG_CHANNEL = -1003746574484
+MONGO_URL = ("mongodb+srv://kayaxrobot:yzSgHQLteJCTXUqK@cluster0.ltk8k8h.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_NAME = ("cluster0")
+OWNER_ID = 8667251104
+ADMIN_URL = os.environ.get("ADMIN_URL", "EternalsHelplineBot")
+BOT_USERNAME = ("MusashiXRobot")
 FSUB_PIC = os.environ.get("FSUB_PIC", "https://files.catbox.moe/bli70r.jpg")
 FSUB_LINK_EXPIRY = int(os.environ.get("FSUB_LINK_EXPIRY", 600))
 START_PIC =os.environ.get("START_PIC", "https://files.catbox.moe/4b8jvw.jpg")
 
 # ─── Filename & Caption Formats ───
 FORMAT = os.environ.get("FORMAT", "[S{season}-E{episode}] {title} [{quality}] [{audio}]")
-CAPTION = os.environ.get("CAPTION", "[ @cantarellabots {FORMAT}]")
+CAPTION = os.environ.get("CAPTION", "<b>{FORMAT}</b>")
 
 # ─── Progress Bar Settings ───
 PROGRESS_BAR = os.environ.get("PROGRESS_BAR", """
@@ -37,9 +37,4 @@ PROGRESS_BAR = os.environ.get("PROGRESS_BAR", """
 # Rotating anime images sent with every bot reply. Add as many as you like.
 RESPONSE_IMAGES = [
     "https://files.catbox.moe/5oonsm.jpg",
-    "https://files.catbox.moe/9ufgme.jpg",
-    "https://files.catbox.moe/4b8jvw.jpg",
-    "https://files.catbox.moe/bli70r.jpg",
-    "https://files.catbox.moe/uce0lw.jpg",
-    "https://files.catbox.moe/is7q4q.jpg"
 ]
